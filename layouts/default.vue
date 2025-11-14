fbd1c2517c692378a3ec8088a1e19920508ec5c5
Aria Aghakhani <48532756+Ariaaghakhani@users.noreply.github.com>
2025-11-15 00:17:35 +0330

    #config/favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,17 +8,7 @@
   </div>
 </template>
 
-<style>
-[dir='rtl'] .nuxt-loading-indicator {
-  transform-origin: right !important;
-}
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.4s;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
-}
-</style>
+<script setup>
+import { useFavicon } from 'assets/composables/useFavicon.js'
+useFavicon()
+</script>
